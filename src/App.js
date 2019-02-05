50a1a6283a9ba1e65dc9715b54055263916a2fe2
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2019-02-04 23:02:45 -0300

    feat: Add route ability to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import "./App.css";
 import * as BooksAPI from "./BooksAPI";
@@ -6,13 +7,6 @@ import BookShelf from "./BookShelf";
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     booksByShelf: {}
   };
 
@@ -39,54 +33,55 @@ class BooksApp extends React.Component {
 
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button
-                className="close-search"
-                onClick={() => this.setState({ showSearchPage: false })}
-              >
-                Close
-              </button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+        <Route
+          exact
+          path="/"
+          component={({ history }) => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  {Object.keys(booksByShelf).map(shelf => (
+                    <BookShelf
+                      key={shelf}
+                      shelf={shelf}
+                      books={booksByShelf[shelf]}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className="open-search">
+                <button onClick={() => history.push("/search")}>
+                  Add a book
+                </button>
+              </div>
+            </div>
+          )}
+        />
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
+        <Route
+          path="/search"
+          component={({ history }) => (
+            <div className="search-books">
+              <div className="search-books-bar">
+                <button
+                  className="close-search"
+                  onClick={() => history.goBack()}
+                >
+                  Close
+                </button>
+                <div className="search-books-input-wrapper">
+                  <input type="text" placeholder="Search by title or author" />
+                </div>
+              </div>
+              <div className="search-books-results">
+                <ol className="books-grid" />
               </div>
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid" />
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                {Object.keys(booksByShelf).map(shelf => (
-                  <BookShelf
-                    key={shelf}
-                    shelf={shelf}
-                    books={booksByShelf[shelf]}
-                  />
-                ))}
-              </div>
-            </div>
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>
-                Add a book
-              </button>
-            </div>
-          </div>
-        )}
+          )}
+        />
       </div>
     );
   }
